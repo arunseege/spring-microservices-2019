095aea4a01f3dc9976712b4bce70758c64ed2929
arunseege <arunsphassan@gmail.com>
2019-08-07 17:13:54 +0530

    added dependency spring-cloud-bus to both limits service and spring cloud config

--- limits-service/src/main/java/com/justcode/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/justcode/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LimitsConfigurationController {
 	
- @Autowired	
+ @Autowired(required=true)
  private  Configuration configuration;
  
 	@GetMapping("/limits")
